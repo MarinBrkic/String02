2cab383d0fe99d40870ab9521ed50165066308f2
MarinBrkic <152974525+MarinBrkic@users.noreply.github.com>
2024-01-16 12:39:56 +0100

    Add files via upload

---
+++ Razmak.cs
@@ -0,0 +1,23 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasaString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1;
+            Console.WriteLine("Unesi neku recenicu: ");
+            s1 = Console.ReadLine();
+
+            Console.WriteLine(s1.Replace(" ", " - "));
+           
+            Console.ReadKey();
+        }
+    }
+}
